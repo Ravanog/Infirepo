835345ec897ef4a5dc9a52328dfb5a38f722ff7a
Ravanog <149065101+Ravanog@users.noreply.github.com>
2024-08-17 10:14:43 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -39,6 +39,7 @@ async def save_file(media):
     try:
         file = Media(
             file_id=file_id,
+            file_ref=file_ref,
             file_name=file_name,
             file_size=media.file_size,
             file_type=media.file_type,
