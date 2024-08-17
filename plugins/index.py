9eab2945617b0ca96af4ebd6b182396d62f37ef0
Ravanog <149065101+Ravanog@users.noreply.github.com>
2024-08-17 11:47:43 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -3,12 +3,14 @@ import asyncio
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified, UserIsBlocked
-from info import ADMINS, LOG_CHANNEL, INDEX_EXTENSIONS
+from info import INDEX_EXTENSIONS as LOG_CHANNEL
+from info import ADMINS
 from database.ia_filterdb import save_file
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import temp, get_readable_time
 import re, time
-
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 lock = asyncio.Lock()
 
 @Client.on_callback_query(filters.regex(r'^index'))
